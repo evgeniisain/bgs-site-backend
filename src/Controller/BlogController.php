0acf5eb90451063aa0d7e65c8c3a84151471ef9c
Evgeny <evgeniisain@mail.ru>
2019-05-13 04:12:24 +1000

    post and comment

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,11 +4,24 @@ namespace App\Controller;
 
 use App\Dto\PostForm;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
+    /**
+     * @param PostForm $postData
+     *
+     * @Route(
+     *     name="blog/update",
+     *     path="/blog/update",
+     *     methods={"GET"}
+     * )
+     *
+     * @return JsonResponse
+     */
     public function update(PostForm $postData)
     {
-
+        return new JsonResponse(['data' => $postData]);
     }
 }
